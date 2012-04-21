4458bf4fd329fc5c7c5386d4e1f6025f6e34d4c2
Daniel Diaz <diaz.carrete@facebook.com>
2012-04-21 16:35:50 +0200

    added some trivial gui code

--- src/main/java/info/danidiaz/xanela/testapp/Main.java
+++ src/main/java/info/danidiaz/xanela/testapp/Main.java
@@ -1,5 +1,8 @@
 package info.danidiaz.xanela.testapp;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,21 @@ public class Main
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new JTextArea(18, 28));
+        //Display the window.
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
