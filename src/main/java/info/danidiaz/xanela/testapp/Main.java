cd01d8e585f4df9f9693ffa390023b4df8bc18f5
Daniel Diaz <diaz.carrete@facebook.com>
2012-05-06 11:16:23 +0200

    more complex test app

--- src/main/java/info/danidiaz/xanela/testapp/Main.java
+++ src/main/java/info/danidiaz/xanela/testapp/Main.java
@@ -1,7 +1,13 @@
 package info.danidiaz.xanela.testapp;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * Hello world!
@@ -24,7 +30,15 @@ public class Main
         //Create and set up the window.
         JFrame frame = new JFrame();
         frame.setTitle("foo frame");
-        frame.getContentPane().add(new JTextArea(18, 28));
+        frame.getContentPane().setLayout(new BorderLayout());        
+        frame.getContentPane().add(new JTextArea(18, 28),BorderLayout.CENTER);
+        frame.getContentPane().add(new JButton("foo"),BorderLayout.SOUTH);
+        
+        JPanel westPanel = new JPanel(new BorderLayout());
+        westPanel.add(new JComboBox(new Object [] { "aaa","bbb","cccc"}), BorderLayout.NORTH);
+        
+        frame.getContentPane().add(westPanel,BorderLayout.EAST);
+        frame.getContentPane().add(new JTextField(18),BorderLayout.NORTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Display the window.
         frame.pack();
